420ce00f4c4248761db2ba3a79892b219c51c85e
elhalili <aminhalili2002@gmail.com>
2023-04-12 02:41:20 +0000

    update baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://creativityFactory.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/SwiftServe-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
